f6423075c7300c2c06664178d7d4897326f37be8
Sebastien Ternisien <sebastienternisien@Sebastiens-Air.home>
2020-08-02 21:08:08 +0200

    new hello world

--- java/Main.java
+++ java/Main.java
@@ -64,6 +64,7 @@ public class Main {
 		System.out.println("-------------------------------------");
 		System.out.println("-------------------------------------");
 		System.out.println("hello world");
+		System.out.println("hello world");
 		return "zizi";
 	}
 
